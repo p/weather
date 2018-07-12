c838078aed3ac31973026fe877f84995a81e69c5
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-12 02:13:15 -0400

    Hot reloading wants to be the exported component, ok

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,6 @@
-import {Router} from 'react-easy-router'
-import {createHashHistory, useBasename} from 'history'
-import routes from './routes'
+import Root from './root'
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { hot } from 'react-hot-loader'
 
-const history = createHashHistory({basename: '/'})
-
-class C extends React.Component {
-  render() {
-    return <Router
-      history={history}
-      routes={routes}/>
-    }
-  }
-
-  const Root = hot(module)(C)
-  
 ReactDOM.render(<Root/>,
 document.getElementById('root'));
---
+++ frontend/src/root.js
@@ -0,0 +1,18 @@
+import {Router} from 'react-easy-router'
+import {createHashHistory, useBasename} from 'history'
+import routes from './routes'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { hot } from 'react-hot-loader'
+
+const history = createHashHistory({basename: '/'})
+
+class Root extends React.Component {
+  render() {
+    return <Router
+      history={history}
+      routes={routes}/>
+    }
+  }
+
+export default hot(module)(Root)
