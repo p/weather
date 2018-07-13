1089e9e203a92e7880b26806f4e42f37102f9d33
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-13 18:15:00 -0400

    Split off wu bits

--- backend/src/server.go
+++ backend/src/server.go
@@ -658,33 +658,6 @@ func get_wu_api_key_cached() (string, error) {
   return api_key, err
 }
 
-type WuForecastResponseMetadata struct {
-  Language string `json:"language"`
-  TransactionId string `json:"transaction_id"`
-  Version string `json:"version"`
-  Latitude float64 `json:"latitude"`
-  Longitude float64 `json:"longitude"`
-  Units string `json:"units"`
-  ExpireTimeGmt int64 `json:"expire_time_gmt"`
-  StatusCode int `json:"status_code"`
-}
-
-type WuForecastResponseForecast struct {
-  Class string `json:"class"`
-  ExpireTimeGmt int64 `json:"expire_time_gmt"`
-  FcstValid int64 `json:"fcst_valid"`
-  FcstValidLocal string `json:"fcst_valid_local"`
-  Num int `json:"num"`
-  MaxTemp json.RawMessage `json:"max_temp"`
-  MinTemp json.RawMessage `json:"min_temp"`
-}
-
-type WuForecastResponse struct {
-  Metadata WuForecastResponseMetadata `json:"metadata"`
-  Forecasts []WuForecastResponseForecast `json:"forecasts"`
-  
-}
-
 func wu_forecast_retriever(resloc resolved_location) (persistable, error) {
   api_key, err := get_wu_api_key_cached()
   if err != nil {
