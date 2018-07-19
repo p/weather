4a8eeeeeb9d56297ded84fd6ed9f69aff1dca7d1
Oleg Pudeyev <oleg@bsdpower.com>
2018-07-18 22:30:27 -0400

    Annotate more

--- backend/src/wu.go
+++ backend/src/wu.go
@@ -29,6 +29,7 @@ type WuForecastResponseDaypart struct {
   FcstValid int64 `json:"fcst_valid"`
   // ISO8601 time for the forecast, e.g. "2018-07-16T19:00:00-0400"
   FcstValidLocal string `json:"fcst_valid_local"`
+  
   // "D" for day, "N" for night
   DayInd string `json:"day_ind"`
   // "Tonight", "Tomorrow", "Wednesday"
@@ -38,6 +39,7 @@ type WuForecastResponseDaypart struct {
   // This is sometimes the same as DaypartName and sometimes same as
   // LongDaypartName
   AltDaypartName string `json:"alt_daypart_name"`
+  
   // Number of this forecast in the returned data, starting with 1.
   // Forecasts for day parts (this struct) and days overall have separate numbering.
   // Forecast for today has num=1. If today only has a night day part,
@@ -46,49 +48,81 @@ type WuForecastResponseDaypart struct {
   // tomorrow's night num=3. The day after tomorrow will have num=3 for the
   // entire day, num=4 for the day part, num=5 for the night part.
   Num                int     `json:"num"`
+  
+  // Max temperature for day day part, min temperature for night day part: 70
   Temp               int     `json:"temp"`
-  Hi                 int     `json:"hi"`
-  Wc                 int     `json:"wc"`
+  // Temperature phrase incorporating whether this is a maximum or minimum
+  // temperature: "Low near 70F.",
+  TempPhrase         string  `json:"temp_phrase"`
+  
+  // Cloud cover in percent? ex: 5, 74
+  Clds               int     `json:"clds"`
+
+  // Precipitation type: "rain"
+  PrecipType         string  `json:"precip_type"`
+  // Probability of precipitation, in pecent: 90
   Pop                int     `json:"pop"`
+  // Probability of precipitation phrase; "Chance of rain 100%."
   PopPhrase          string  `json:"pop_phrase"`
+
+  // Thunder possibility flag: 0-2
+  ThunderEnum        int     `json:"thunder_enum"`
+  // Thunder possibility phrase:
+  // ThunderEnum=0: "No thunder"
+  // ThunderEnum=1: "Thunder possible"
+  // ThunderEnum=2: "Thunder expected"
+  ThunderEnumPhrase  string  `json:"thunder_enum_phrase"`
+
+  // Wind speed: 12
+  // This should be within the wind speed range given by the narrative
+  Wspd               int     `json:"wspd"`
+  // Wind direction in degrees: 211
+  Wdir               int     `json:"wdir"`
+  // Wind direction as a string: SSW
+  WdirCardinal       string  `json:"wdir_cardinal"`
+  // Wind information phrase: "Winds SSW at 10 to 15 mph."
+  WindPhrase         string  `json:"wind_phrase"`
+
   IconExtd           int     `json:"icon_extd"`
   IconCode           int     `json:"icon_code"`
-  Wxman              string  `json:"wxman"`
   Phrase12Char       string  `json:"phrase_12char"`
   Phrase22Char       string  `json:"phrase_22char"`
   Phrase32Char       string  `json:"phrase_32char"`
   SubphrasePt1       string  `json:"subphrase_pt1"`
   SubphrasePt2       string  `json:"subphrase_pt2"`
   SubphrasePt3       string  `json:"subphrase_pt3"`
-  PrecipType         string  `json:"precip_type"`
-  Rh                 int     `json:"rh"`
-  Wspd               int     `json:"wspd"`
-  Wdir               int     `json:"wdir"`
-  WdirCardinal       string  `json:"wdir_cardinal"`
-  Clds               int     `json:"clds"`
-  TempPhrase         string  `json:"temp_phrase"`
-  AccumulationPhrase string  `json:"accumulation_phrase"`
-  WindPhrase         string  `json:"wind_phrase"`
   Shortcast          string  `json:"shortcast"`
   Narrative          string  `json:"narrative"`
-  ThunderEnum        int     `json:"thunder_enum"`
-  ThunderEnumPhrase  string  `json:"thunder_enum_phrase"`
   Qpf                float64 `json:"qpf"`
   // may be int
   SnowQpf    float64 `json:"snow_qpf"`
   SnowRange  string  `json:"snow_range"`
   SnowPhrase string  `json:"snow_phrase"`
   SnowCode   string  `json:"snow_code"`
-  VocalKey   string  `json:"vocal_key"`
+  // Snow accumulation?
+  AccumulationPhrase string  `json:"accumulation_phrase"`
   // this was always null, don't know type
   QualifierCode *string `json:"wind_phrase"`
+  // ex: "A stray shower or thunderstorm is possible."
   Qualifier     *string `json:"qualifier"`
+  
+  // ex: 7.9
   UvIndexRaw    float64 `json:"uv_index_raw"`
+  // UvIndexRaw rounded to an integer? ex: 8
   UvIndex       int     `json:"uv_index"`
   UvWarning     int     `json:"uv_warning"`
+  // ex: "Very High"
   UvDesc        string  `json:"uv_desc"`
+  
   GolfIndex     int     `json:"golf_index"`
   GolfCategory  string  `json:"golf_category"`
+
+  // ex: "wx1600"
+  Wxman              string  `json:"wxman"`
+  Hi                 int     `json:"hi"`
+  Wc                 int     `json:"wc"`
+  Rh                 int     `json:"rh"`
+  VocalKey   string  `json:"vocal_key"`
 }
 
 type WuForecastResponseForecast struct {
@@ -100,6 +134,8 @@ type WuForecastResponseForecast struct {
   FcstValid int64 `json:"fcst_valid"`
   // ISO8601 time: "2018-07-18T07:00:00-0400"
   FcstValidLocal string `json:"fcst_valid_local"`
+  // Day of week, e.g. "Monday", "Tuesday"
+  Dow            string `json:"dow"`
   // Number of this forecast in the returned data, starting with 1.
   // Forecasts for days (this struct) and day parts have separate numbering.
   // Forecast for today has num=1. If today only has a night day part,
@@ -121,18 +157,19 @@ type WuForecastResponseForecast struct {
   Blurb *string `json:"blurb"`
   // was always null
   BlurbAuthor   *string `json:"blurb_author"`
+  // ex: 5
   LunarPhaseDay int     `json:"lunar_phase_day"`
-  // Day of week, e.g. "Monday", "Tuesday"
-  Dow            string `json:"dow"`
+  // ex: "Waxing Crescent"
   LunarPhase     string `json:"lunar_phase"`
+  // ex: "WXC"
   LunarPhaseCode string `json:"lunar_phase_code"`
   Sunrise        string `json:"sunrise"`
   Sunset         string `json:"sunset"`
   Moonrise       string `json:"moonrise"`
   Moonset        string `json:"moonset"`
-  // assume *string
+  // assuming *string
   QualifierCode *string `json:"qualifier_code"`
-  // assume *string
+  // assuming *string
   Qualifier string  `json:"qualifier"`
   Narrative string  `json:"narrative"`
   Qpf       float64 `json:"qpf"`
