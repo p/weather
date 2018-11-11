580f0376ebd36bce046e1213654b9ca50171b530
Oleg Pudeyev <oleg@bsdpower.com>
2018-11-11 13:18:05 -0500

    No network detection in production

--- frontend/src/actions.js
+++ frontend/src/actions.js
@@ -2,11 +2,17 @@ import reactor from './reactor'
 
 export default {
   fetch_network(){
+    if(NODE_ENV!='production'){
+      setTimeout(function(){
+      reactor.dispatch('receive_network',{up:true})
+    },0)
+    }else{
     fetch(API_URL + '/network')
       .then(resp => resp.json())
       .then(payload => {
         reactor.dispatch('receive_network', payload)
       })
+    }
   },
   
   fetch_forecast(location_query,network_flag){
