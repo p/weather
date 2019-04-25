d94d7452fd4b59b414942d6bfae1448ebefb914e
Oleg Pudeyev <oleg@bsdpower.com>
2019-04-25 11:22:20 -0400

    extract into view

--- frontend/src/components/full-location.js
+++ frontend/src/components/full-location.js
@@ -1,3 +1,4 @@
+import FullLocationView from '../views/full-location'
 import { connect } from 'nuclear-js-react-addons-chefsplate'
 import { network_flag, unim } from '../util'
 import { data_age } from '../util'
@@ -10,109 +11,25 @@ import _ from 'underscore'
 import React from 'react'
 import Store from '../store'
 import Current from '../components/current'
+import { mapProps } from '@rq/react-map-props'
 
-class FullLocationImpl extends React.Component {
-  render() {
-    //console.log(this.props.forecast)
-    return (
-      <div>
-        <h2>
-          {this.props.location
-            ? this.props.location.city + ', ' + this.props.location.state
-            : this.props.location_query}
-        </h2>
-
-        {this.props.current && <Current current={this.props.current} />}
-
-        {this.props.forecast && (
-          <div>
-            {_.map(this.props.forecast.daily_forecasts, forecast => (
-              <div className="forecast-row" key={forecast.time}>
-                <div className="forecast-date">
-                  <div>{moment(forecast.time * 1000).format('dddd')}</div>
-                  <div>{moment(forecast.time * 1000).format('MMM D')}</div>
-                </div>
-
-                {forecast.day &&
-                  this.render_day_part_forecast('day', forecast.day)}
-                {this.render_day_part_forecast('night', forecast.night)}
-              </div>
-            ))}
-            <p>Updated: {data_age(this.props.forecast)}</p>
-          </div>
-        )}
-      </div>
-    )
-  }
-
-  render_day_part_forecast(day_part_name, forecast) {
-    return (
-      <div className={'forecast-' + day_part_name}>
-        <div className="forecast-temp">{forecast.temp.toString() + '\xb0'}</div>
-        <div className="forecast-precip">
-          {forecast.precip_probability ? (
-            <div>
-              <div>{forecast.precip_probability}%</div>
-              <div>{forecast.precip_type}</div>
-            </div>
-          ) : (
-            ''
-          )}
-        </div>
-        <div className="forecast-blurb">{forecast.narrative}</div>
-      </div>
-    )
-  }
-
-  format_short_forecast(name, dpf) {
-    return (
-      <p>
-        {name}:{' '}
-        <b>
-          {dpf.temp}&deg;, {dpf.precip_type}: {dpf.precip_probability}%
-        </b>{' '}
-        {dpf.narrative}
-      </p>
-    )
-  }
-}
-
-FullLocationImpl.propTypes = {
-  locationQuery: PropTypes.string.isRequired,
-  
-  location: PropTypes.shape({
-    city: PropTypes.string.isRequired,
-    state_abbr: PropTypes.string.isRequired,
-  }),
-  
-  forecast: PropTypes.shape({
-    daily_forecasts: PropTypes.arrayOf(PropTypes.shape({
-      // UTC timestamp
-      time: PropTypes.number.isRequired,
-      // UTC timestamp
-      updated_at: PropTypes.number.isRequired,
-      day: PropTYpes.shape({
-        temp: PropTypes.number.isRequired,
-        precip_probability: PropTypes.number.isRequired,
-        precip_type: PropTypes.string.isRequired,
-      }),
-      night: PropTYpes.shape({
-        temp: PropTypes.number.isRequired,
-        precip_probability: PropTypes.number.isRequired,
-        precip_type: PropTypes.string.isRequired,
-      }),
-    }),
-  })),
-  
-  current: Current.propTypes.current,
-}
-
-export default const FullLocation = connect(props => ({
+@connect(props => ({
   forecast: [['forecast', props.location_query, 'forecast'], unim],
   current: [['forecast', props.location_query, 'current'], unim],
   location: [['forecast', props.location_query, 'location'], unim],
-}))(FullLocationImpl)
+}))
+export default class FullLocation extends React.Component {
+  render() {
+    return (
+      <FullLocationView
+    location_query={this.props.location_query}
+    forecast={this.props.forecast}
+    current={this.props.current}
+    location={this.props.location}
+    />
+  }
+}
 
 FullLocation.propTypes = {
-  locationQuery: PropTypes.string.isRequired,
+  location_query: PropTypes.string.isRequired,
 }
---
+++ frontend/src/views/full-location.js
@@ -0,0 +1,106 @@
+import { network_flag, unim } from '../util'
+import { data_age } from '../util'
+import PropTypes from 'prop-types'
+import moment from 'moment'
+import { Link } from '@rq/react-easy-router'
+import Immutable from 'seamless-immutable'
+import preventDefaultWrapper from '@rq/prevent-default-wrapper'
+import _ from 'underscore'
+import React from 'react'
+import Store from '../store'
+import Current from '../components/current'
+
+class FullLocationView extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          {this.props.location
+            ? this.props.location.city + ', ' + this.props.location.state
+            : this.props.location_query}
+        </h2>
+
+        {this.props.current && <Current current={this.props.current} />}
+
+        {this.props.forecast && (
+          <div>
+            {_.map(this.props.forecast.daily_forecasts, forecast => (
+              <div className="forecast-row" key={forecast.time}>
+                <div className="forecast-date">
+                  <div>{moment(forecast.time * 1000).format('dddd')}</div>
+                  <div>{moment(forecast.time * 1000).format('MMM D')}</div>
+                </div>
+
+                {forecast.day &&
+                  this.render_day_part_forecast('day', forecast.day)}
+                {this.render_day_part_forecast('night', forecast.night)}
+              </div>
+            ))}
+            <p>Updated: {data_age(this.props.forecast)}</p>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  render_day_part_forecast(day_part_name, forecast) {
+    return (
+      <div className={'forecast-' + day_part_name}>
+        <div className="forecast-temp">{forecast.temp.toString() + '\xb0'}</div>
+        <div className="forecast-precip">
+          {forecast.precip_probability ? (
+            <div>
+              <div>{forecast.precip_probability}%</div>
+              <div>{forecast.precip_type}</div>
+            </div>
+          ) : (
+            ''
+          )}
+        </div>
+        <div className="forecast-blurb">{forecast.narrative}</div>
+      </div>
+    )
+  }
+
+  format_short_forecast(name, dpf) {
+    return (
+      <p>
+        {name}:{' '}
+        <b>
+          {dpf.temp}&deg;, {dpf.precip_type}: {dpf.precip_probability}%
+        </b>{' '}
+        {dpf.narrative}
+      </p>
+    )
+  }
+}
+
+FullLocationImpl.propTypes = {
+  location_query: PropTypes.string.isRequired,
+  
+  location: PropTypes.shape({
+    city: PropTypes.string.isRequired,
+    state_abbr: PropTypes.string.isRequired,
+  }),
+  
+  forecast: PropTypes.shape({
+    daily_forecasts: PropTypes.arrayOf(PropTypes.shape({
+      // UTC timestamp
+      time: PropTypes.number.isRequired,
+      // UTC timestamp
+      updated_at: PropTypes.number.isRequired,
+      day: PropTYpes.shape({
+        temp: PropTypes.number.isRequired,
+        precip_probability: PropTypes.number.isRequired,
+        precip_type: PropTypes.string.isRequired,
+      }),
+      night: PropTYpes.shape({
+        temp: PropTypes.number.isRequired,
+        precip_probability: PropTypes.number.isRequired,
+        precip_type: PropTypes.string.isRequired,
+      }),
+    }),
+  })),
+  
+  current: Current.propTypes.current,
+}
