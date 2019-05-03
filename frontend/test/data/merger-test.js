debbf5641cdbc7c43f9416fd0e87be1e97a27878
Oleg Pudeyev <oleg@bsdpower.com>
2019-05-02 21:05:15 -0400

    fmt test

--- frontend/test/data/merger-test.js
+++ frontend/test/data/merger-test.js
@@ -1,49 +1,54 @@
-import {assert} from 'chai'
+import { assert } from 'chai'
 
-import {merge_hourly_into_daily_forecasts} from '../../src/data/merger'
+import { merge_hourly_into_daily_forecasts } from '../../src/data/merger'
 
 const mhd = merge_hourly_into_daily_forecasts
 
-
 describe('merge_hourly_into_daily_forecasts', function() {
-  describe('1 daily & 1 hourly', ()=>{
-const daily_fcs = [{
-  start_timestamp: 1,
-  expire_timestamp: 1,
-  day: {
-    start_timestamp: 1,
-    temp: 20,
-  },
-  night: {
-    start_timestamp: 2,
-    temp: 20,
-  },
-}]
+  describe('1 daily & 1 hourly', () => {
+    const daily_fcs = [
+      {
+        start_timestamp: 1,
+        expire_timestamp: 1,
+        day: {
+          start_timestamp: 1,
+          temp: 20,
+        },
+        night: {
+          start_timestamp: 2,
+          temp: 20,
+        },
+      },
+    ]
 
-const hourly_fcs = [{
-  expire_timestamp: 1,
-  start_timestamp: 1,
-  temp: 21,
-}]
+    const hourly_fcs = [
+      {
+        expire_timestamp: 1,
+        start_timestamp: 1,
+        temp: 21,
+      },
+    ]
 
-const expected=[{
-  start_timestamp: 1,
-  expire_timestamp: 1,
-  day: {
-    start_timestamp: 1,
-    temp: 20,
-  hourly: hourly_fcs,
-  },
-  night: {
-    start_timestamp: 2,
-    temp: 20,
-    hourly:[],
-  },
-}]
-  
+    const expected = [
+      {
+        start_timestamp: 1,
+        expire_timestamp: 1,
+        day: {
+          start_timestamp: 1,
+          temp: 20,
+          hourly: hourly_fcs,
+        },
+        night: {
+          start_timestamp: 2,
+          temp: 20,
+          hourly: [],
+        },
+      },
+    ]
+
     it('works', function() {
-      let actual=mhd(daily_fcs,hourly_fcs)
-      assert.deepEqual(actual,expected)
-    });
+      let actual = mhd(daily_fcs, hourly_fcs)
+      assert.deepEqual(actual, expected)
+    })
   })
-});
+})
